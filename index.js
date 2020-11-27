b97dba603ece86fa4947d61dcc86e1c433c0c371
chiefsmurph <chiefsmurph@gmail.com>
2020-11-26 20:08:59 -0800

    log it

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const socketIOClient = require('socket.io-client');
 const { rhEndpoint, options } = require('./config');
 
+
+const rhSocket = socketIOClient(rhEndpoint, options);
+
 const getData = require('./get-data');
 
 
@@ -19,8 +22,10 @@ app.options('*', cors());
 app.get('*', async (req, res) => {
   const ip = req.header('cf-connecting-ip') || (req.header('x-forwarded-for') || req.connection.remoteAddress).split(', ').pop();
   const userAgent = req.headers['user-agent'];
+  const logStr = log => rhSocket.emit('client:act', 'log', log, { ip, userAgent });
   if (userAgent.toLowerCase().includes('bot')) return {};
   console.log({ ip });
   const data = await getData(ip);
+  logStr(`weather-service: ${Object.entries({ ip, ...data }).map(([key, value]) => [key, value].join(': ')).join(' and ')}`);
   res.json(data);
 });
